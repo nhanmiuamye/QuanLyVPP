603243b5ffd71845db23fed10f9496a53c81687f
Phạm Doãn Nguyên Tùng <68810319+nhanmiuamye@users.noreply.github.com>
2024-06-04 13:48:27 +0700

    Add files via upload
    Upload code

---
+++ Django.py
@@ -0,0 +1,40 @@
+"""
+
+Django is the Free and open source Web application Framework written in Python
+Django is a high-level Python web framework that was designed for rapid web development,
+
+It follows the Model-View-Controller (MVC) architectural pattern,
+where 
+the model defines the database schema and data access logic,
+
+the view handles the user interface, 
+
+and the controller handles user input and business logic.
+
+It offers the big collection of modules which are easy to use 
+It saves the programmers time and headache a lot
+
+
+# The MVT architechture
+
+# MVT= Model View Template 
+
+
+There is no Separate Controller in Django
+The Entire Application is based on This MVT Architechture
+
+it provides a wide range of tools and features out of the box, 
+including an ORM (Object-Relational Mapping) system, 
+URL routing, template engine, authentication, and administration interfaces.
+
+View ==> It use to execute the logic and interact with a model to carry data and renders the template
+
+                                                        (Model)
+                                                         //                 
+                                                        //
+(User) ======  (DjangoAppLication) ======= (URL) =======
+                                                        \\
+                                                         \\
+                                                        (Template)
+
+"""
